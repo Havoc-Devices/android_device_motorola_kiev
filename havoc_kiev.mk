c8d359a037871ba5c715a5c90570a81aa952c4e9
Anushek Prasal <anushekprasal@gmail.com>
2022-01-17 18:21:23 +0530

    kiev: Update product brand name
    Change-Id: I1acfd59eddf96592ab3328dd10aafb2665381b52

--- havoc_kiev.mk
+++ havoc_kiev.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := havoc_kiev
 PRODUCT_DEVICE := kiev
-PRODUCT_BRAND := motorola
+PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := moto g 5G
 PRODUCT_MANUFACTURER := motorola
 
